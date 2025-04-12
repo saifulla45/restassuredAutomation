7823e1b22265360ae60e805f28ca3e61d5791263
saifulla45 <saifulla.attar5227@gmail.com>
2025-04-12 15:15:58 +0530

    initial commit

---
+++ src/main/java/restassured_Api_Automation/com/restassured/apiautomation/App.java
@@ -0,0 +1,13 @@
+package restassured_Api_Automation.com.restassured.apiautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
